0d4fda9d6345755e157a256fd33d48556c5f4a7a
Saber Haj Rabiee <sabify@users.noreply.github.com>
2019-05-31 07:37:11 -0700

    Adding Bytes() to get slice representation of ULID (#49)
    * Adding Bytes() to get slice representation of ULID
    
    * Added Test For Bytes()
    
    * Added The Test For Bytes()

--- ulid_test.go
+++ ulid_test.go
@@ -619,6 +619,17 @@ func TestMonotonicSafe(t *testing.T) {
 	}
 }
 
+func TestULID_Bytes(t *testing.T) {
+	tt := time.Unix(1000000, 0)
+	entropy := ulid.Monotonic(rand.New(rand.NewSource(tt.UnixNano())), 0)
+	id := ulid.MustNew(ulid.Timestamp(tt), entropy)
+	bid := id.Bytes()
+	bid[len(bid)-1]++
+	if bytes.Equal(id.Bytes(), bid) {
+		t.Error("Bytes() returned a reference to ulid underlying array!")
+	}
+}
+
 type safeMonotonicReader struct {
 	mtx sync.Mutex
 	ulid.MonotonicReader
