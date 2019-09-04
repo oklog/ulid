3c338c0c98b40f468f858cdb29c644f997ef2055
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-09-04 01:49:11 -0400

    Fuzz monotonic entropy construction

--- ulid_fuzz.go
+++ ulid_fuzz.go
@@ -7,14 +7,9 @@ import (
 	"encoding/binary"
 )
 
-// FuzzNew tests ULID construction with fuzzed timestamp and entropy.
+// FuzzNew tests ULID construction.
 func FuzzNew(fuzz []byte) int {
-	var ms uint64
-	binary.Read(bytes.NewReader(fuzz), binary.LittleEndian, &ms)
-	var entropy []byte
-	if len(fuzz) > 8 {
-		entropy = fuzz[7:]
-	}
+	ms, entropy := extractFuzzTimestamp(fuzz)
 	id, err := New(ms, bytes.NewReader(entropy))
 	if err != nil {
 		return 0
@@ -29,6 +24,41 @@ func FuzzNew(fuzz []byte) int {
 	_, err = id.MarshalBinary()
 	if err != nil {
 		return 0
+	}
+	return 1
+}
+
+// FuzzNewMonotonic tests ULID construction with monotonic entropy.
+func FuzzNewMonotonic(fuzz []byte) int {
+	if len(fuzz) < (8 * 10) {
+		return -1
+	}
+	timestamps := fuzz[0 : 8*10]
+	var entropy []byte
+	if len(fuzz) > (8 * 10) {
+		entropy = fuzz[8*10:]
+	} else {
+		entropy = []byte{}
+	}
+	monotonic := Monotonic(bytes.NewReader(entropy), 0)
+	var ms uint64
+	for range [10]struct{}{} {
+		ms, timestamps = extractFuzzTimestamp(timestamps)
+		id, err := New(ms, monotonic)
+		if err != nil {
+			return 0
+		}
+		id.Bytes()
+		id.Entropy()
+		id.Time()
+		_, err = id.MarshalText()
+		if err != nil {
+			return 0
+		}
+		_, err = id.MarshalBinary()
+		if err != nil {
+			return 0
+		}
 	}
 	return 1
 }
@@ -71,4 +101,14 @@ func FuzzParseStrict(fuzz []byte) int {
 		return 0
 	}
 	return 1
+}
+
+func extractFuzzTimestamp(fuzz []byte) (ms uint64, rest []byte) {
+	binary.Read(bytes.NewReader(fuzz), binary.LittleEndian, &ms)
+	if len(fuzz) > 8 {
+		rest = fuzz[7:]
+	} else {
+		rest = []byte{}
+	}
+	return
 }
